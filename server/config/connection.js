72490c84a3914938e844b677bed5669b1c99fb39
Edwin Fame <edwinfame@yahoo.com>
2022-06-01 15:16:18 -0700

    fixed index

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/BookFinderApp', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bookfinderapp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
